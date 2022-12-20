8c17cf289ad16620507bf587dd390242014aeac8
Titihipong <rockqr.rocker@gmail.com>
2022-12-20 15:33:56 +0700

    up.1

--- csmju.js
+++ csmju.js
@@ -1,6 +1,14 @@
 function calbmi() {
     var height = 172;
     var weight = 70;
-    console.log(height)
-    return + weight / (height /100)**2;
+
+    //   console.log(height);
+
+    return + weight / (height / 100) ** 2;
+}
+
+function logN(n) {
+    for (var i = 1; i <= 10; i++) {
+        console.log(i);
+    }
 }
